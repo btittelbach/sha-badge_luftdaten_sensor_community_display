07d325f044943baae16989666d1d86ef789f9120
Bernhard Tittelbach <bernhard@tittelbach.org>
2022-04-05 12:14:28 +0200

    indent using 4 spaces, which unfortunately uses more µC flash space

--- __init__.py
+++ __init__.py
@@ -7,7 +7,6 @@ import time
 import display
 
 
-
 sc_pm_sensor_id_ = int(badge.nvs_get_str(
     "sensorcommunity",
     "sid",
@@ -21,18 +20,18 @@ sc_update_interval_ = 250
 sensordata_ = {}
 
 weathericons = {
-	"arrowup": "\xf0\x58",
-	"arrowdown": "\xf0\x44",
-	"thermometer": "\xf0\x55",
-	"cloud": "\xf0\x41",
-	"degree": "\xf0\x42",
-	"degC": "\xf0\x3c",
+    "arrowup": "\xf0\x58",
+    "arrowdown": "\xf0\x44",
+    "thermometer": "\xf0\x55",
+    "cloud": "\xf0\x41",
+    "degree": "\xf0\x42",
+    "degC": "\xf0\x3c",
 }
 
 fonticons = {
-	"degC":"\x21\x03",
-	"arrowup": "\x2b\x4e",
-	"arrowdown": "\x2b\x4f"
+    "degC":"\x21\x03",
+    "arrowup": "\x2b\x4e",
+    "arrowdown": "\x2b\x4f"
 }
 
 def clearRect(rect):
@@ -52,61 +51,61 @@ def drawLabel(rect, type_max_width, stype, svalue):
     # display.drawText(x + w//2 - label_w//2, y + h//2 - label_h//2, label, 0)
 
 def printSensorData():
-	for stype, svalue in sensordata_.items():
-		print("{}: {}".format(stype, svalue))
+    for stype, svalue in sensordata_.items():
+        print("{}: {}".format(stype, svalue))
 
 def displaySensorData():
-	if (len(sensordata_) < 1):
-		clearRect(0,0,display.width(),display.height())
-		display.drawText(2, 2, "X", 0, "permanentmarker22", 2, 2)
-		display.flush()
-		return
-	sdh = (display.height()-2) // len(sensordata_)
-	y = 0
-	type_max_width = max([ display.getTextWidth(s) for s in sensordata_.keys() ])
-	for stype, svalue in sensordata_.items():
-		drawLabel([2, y, display.width()-4, sdh], type_max_width, stype, str(svalue))
-		y += sdh
-	display.flush()
+    if (len(sensordata_) < 1):
+        clearRect(0,0,display.width(),display.height())
+        display.drawText(2, 2, "X", 0, "permanentmarker22", 2, 2)
+        display.flush()
+        return
+    sdh = (display.height()-2) // len(sensordata_)
+    y = 0
+    type_max_width = max([ display.getTextWidth(s) for s in sensordata_.keys() ])
+    for stype, svalue in sensordata_.items():
+        drawLabel([2, y, display.width()-4, sdh], type_max_width, stype, str(svalue))
+        y += sdh
+    display.flush()
 
 def getSensorData(sids_list):
-	for sid in sids_list:
-		# try:
-		uri = sc_api_url_.format(sensorid=sid)
-		apiConnection = urequests.get(url=uri)
-		if not apiConnection:
-			print("httprequest failed")
-			continue
-		jsonData = apiConnection.json()
-		time.sleep(0.2)
-		apiConnection.close()
-		for sdv in jsonData[1]["sensordatavalues"]:
-			sensordata_[sdv["value_type"]] = sdv["value"]
-		# except:
-		# 	print("getSensorData failed")
-		# 	api_fail_ct_ = api_fail_ct_ +1
-		# 	if (api_fail_ct_ > 4):
-		# 		sensordata_ = {} # delete outdated sensordata
-	sensordata_["Time"] = time.strftime("%H:%M  %Y-%m-%d UTC")
+    for sid in sids_list:
+        # try:
+        uri = sc_api_url_.format(sensorid=sid)
+        apiConnection = urequests.get(url=uri)
+        if not apiConnection:
+            print("httprequest failed")
+            continue
+        jsonData = apiConnection.json()
+        time.sleep(0.2)
+        apiConnection.close()
+        for sdv in jsonData[1]["sensordatavalues"]:
+            sensordata_[sdv["value_type"]] = sdv["value"]
+        # except:
+        #   print("getSensorData failed")
+        #   api_fail_ct_ = api_fail_ct_ +1
+        #   if (api_fail_ct_ > 4):
+        #       sensordata_ = {} # delete outdated sensordata
+    sensordata_["Time"] = time.strftime("%H:%M  %Y-%m-%d UTC")
 
 def loop():
-	if not wifi.status():
-		print("connecting to WiFi")
-		wifi.connect()
-	if not wifi.wait(6):
-		print("WiFi wait timed out")
-		return sc_update_interval_ * 4
+    if not wifi.status():
+        print("connecting to WiFi")
+        wifi.connect()
+    if not wifi.wait(6):
+        print("WiFi wait timed out")
+        return sc_update_interval_ * 4
 
-	wifi.ntp()
-	#get sensordata
-	print("getting data")
-	getSensorData([sc_pm_sensor_id_, sc_env_sensor_id_])
-	print("printing data")
-	printSensorData()
-	print("rendering data on epaper")
-	displaySensorData()
-	return sc_update_interval_
+    wifi.ntp()
+    #get sensordata
+    print("getting data")
+    getSensorData([sc_pm_sensor_id_, sc_env_sensor_id_])
+    print("printing data")
+    printSensorData()
+    print("rendering data on epaper")
+    displaySensorData()
+    return sc_update_interval_
 
 
 while True:
-	time.sleep(loop())
+    time.sleep(loop())
