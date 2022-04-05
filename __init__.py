023caaf092e2e55a909570c0bd51e28ce8a897d7
Bernhard Tittelbach <bernhard@tittelbach.org>
2022-04-05 15:38:26 +0200

    experimenting with buttons

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ import time
 import display
 import buttons
 import system
-import mutex
+from collections import namedtuple
 
 sc_pm_sensor_id_ = int(badge.nvs_get_str(
     "sensorcommunity",
@@ -18,10 +18,12 @@ sc_env_sensor_id_ = sc_pm_sensor_id_ +1
 sc_api_url_ = "https://data.sensor.community/airrohr/v1/sensor/{sensorid}/"
 sc_update_interval_ = 250
 
-loop_reentrance_avoidance_lock_ = mutex.Mutex()
+loop_reentrance_avoidance_lock_ = False ## in absence of mutex module
 
+SensorTuple = namedtuple("SensorTuple",("value","unit","trend"))
 sensordata_ = {}
 
+weatherfont = "weather42"
 weathericons = {
     "arrowup": "\xf0\x58",
     "arrowdown": "\xf0\x44",
@@ -31,7 +33,7 @@ weathericons = {
     "degC": "\xf0\x3c",
 }
 
-usefont = "roboto"
+usefont = "roboto_regular12"
 fonticons = {
     "degC":"\x21\x03",
     "arrowup": "\x2b\x4e",
@@ -68,7 +70,7 @@ def displaySensorData():
     y = 0
     type_max_width = max([ display.getTextWidth(s) for s in sensordata_.keys() ])
     for stype, svalue in sensordata_.items():
-        drawLabel([2, y, display.width()-4, sdh], type_max_width, stype, str(svalue))
+        drawLabel([2, y, display.width()-4, sdh], type_max_width, stype, str(svalue.value)+str(svalue.unit)+" "+str(svalue.trend))
         y += sdh
     display.flush()
 
@@ -81,20 +83,46 @@ def getSensorData(sids_list):
             print("httprequest failed")
             continue
         jsonData = apiConnection.json()
-        time.sleep(0.2)
         apiConnection.close()
-        for sdv in jsonData[1]["sensordatavalues"]:
-            sensordata_[sdv["value_type"]] = sdv["value"]
+        if len(jsonData) > 1:
+            if "sensordatavalues" in jsonData[0]:
+                for sdv in jsonData[0]["sensordatavalues"]:
+                    unit=""
+                    if sdv["value_type"] == "temperature":
+                        unit="°C"
+                    elif sdv["value_type"] == "pressure" or sdv["value_type"] == "pressure_at_sealevel":
+                        unit="Pa"
+                    elif sdv["value_type"] == "humidity":
+                        unit="%"
+                    elif sdv["value_type"] == "P1" or sdv["value_type"] == "P2":
+                        unit="ug/m³"
+                    sensordata_[sdv["value_type"]] = SensorTuple(value=sdv["value"], unit=unit, trend="")
+        if len(jsonData) > 2:
+            if "sensordatavalues" in jsonData[-1]:
+                for sdv in jsonData[-1]["sensordatavalues"]:
+                    if sensordata_[sdv["value_type"]].value > sdv["value"] +1:
+                        sensordata_[sdv["value_type"]].trend = fonticons["arrowup"]
+                    elif sensordata_[sdv["value_type"]].value < sdv["value"] -1:
+                        sensordata_[sdv["value_type"]].trend = fonticons["arrowdown"]
+
         # except:
         #   print("getSensorData failed")
         #   api_fail_ct_ = api_fail_ct_ +1
         #   if (api_fail_ct_ > 4):
         #       sensordata_ = {} # delete outdated sensordata
-    sensordata_["Time"] = time.strftime("%H:%M  %Y-%m-%d UTC")
+    sensordata_["Time"] = SensorTuple(time.strftime("%H:%M  %Y-%m-%d UTC"),"","")
+
+def displayMsg(msg):
+    label_h = display.getTextHeight(msg)
+    y = display.height()-label_h
+    clearRect([0,y,display.width(),label_h])
+    display.drawText(0, y, msg, 0, usefont)
 
 def loop():
-    if not loop_reentrance_avoidance_lock_.test():
+    global loop_reentrance_avoidance_lock_
+    if loop_reentrance_avoidance_lock_:
         return 3
+    loop_reentrance_avoidance_lock_ = True
     if not wifi.status():
         print("connecting to WiFi")
         wifi.connect()
@@ -110,15 +138,20 @@ def loop():
     printSensorData()
     print("rendering data on epaper")
     displaySensorData()
-    loop_reentrance_avoidance_lock_.release()
+    loop_reentrance_avoidance_lock_ = False
     return sc_update_interval_
 
-def buttonExitApp():
+def buttonExitApp(pressed):
     system.home()
 
-button.attach("A", loop)
-button.attach("B", loop)
-button.attach("START", buttonExitApp)
+def buttonForceUpdate(pressed):
+    displayMsg("updating ...")
+    loop()
 
+buttons.attach(buttons.BTN_A, buttonForceUpdate)
+buttons.attach(buttons.BTN_B, buttonForceUpdate)
+buttons.attach(buttons.BTN_START, buttonExitApp)
+
+displayMsg("getting sensordata from data.sensor.community ...")
 while True:
-    time.sleep(loop())
+    system.sleep(loop()*1000)
